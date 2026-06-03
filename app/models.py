05c90d9f5c69c90dc26fa468ab1f995df0a29f91
nyrislam <nyrikesheihe@gmail.com>
2026-06-03 14:20:13 +0300

    Init project (No ORM)

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, text
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    gmail = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=False),
+        nullable=False,
+        server_default=text("now()")
+    )
