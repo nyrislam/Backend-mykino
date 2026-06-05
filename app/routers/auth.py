bff383163d0efd3bceb01d7ae852d90fd802f441
nyrislam <nyrikesheihe@gmail.com>
2026-06-06 00:42:07 +0300

    add OAUTH2 passwordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from .. import models, schemas, utils, oauth2
 from ..database import Session, get_db
 
@@ -7,8 +8,8 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def find_login(login: schemas.UserLogin, db: Session = Depends(get_db)):
-    get_user = db.query(models.User).filter(models.User.email == login.email).first()
+def find_login(login: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    get_user = db.query(models.User).filter(models.User.email == login.username).first()
     if not get_user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"инвалид")
     if not utils.verify(login.password, get_user.password): # body == db
@@ -16,4 +17,4 @@ def find_login(login: schemas.UserLogin, db: Session = Depends(get_db)):
     
     access_token = oauth2.create_access_token(data={"user_id": get_user.id})
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "data": get_user}
